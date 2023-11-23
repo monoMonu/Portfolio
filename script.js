883036d8d9a3b9952e96bc0d4a91fdb6f396d7b0
Sonu Singh <ms88006218@gmail.com>
2023-11-23 23:45:46 +0530

    Update

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
+
 const modeSwitch = document.querySelector(".mode-switch");
 const firstPage = document.querySelector(".firstPage");
+const r = document.querySelector(':root');
 
 
-modeSwitch.addEventListener("click", changeMode());
 function changeMode(){
-   let root = document.querySelector(":root");
-   let rv = getComputedStyle(root);
-   document.write(9);
+   
    if(modeSwitch.classList.contains("light")){
-      rv.style.setProperty("--theme1","#edf");
-      rv.style.setProperty("--theme2","#22222b");
-      rv.style.setProperty("--theme1","#edf");
+      r.style.setProperty("--theme1","#edf");
+      r.style.setProperty("--theme2","#22222b");
+      r.style.setProperty("--theme1","#edf");
    }
    else{
-      rv.style.setProperty("--theme1","#22222b");
-      rv.style.setProperty("--theme2","#edf");
+      r.style.setProperty("--theme2", "#22222b");
+      r.style.setProperty("--theme1","#edf");
    }
 }
-alert(9);
+modeSwitch.addEventListener("click", changeMode);
+
+
