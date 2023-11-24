f99263275b349c73d6b065d100acb85b5fd8b29f
Sonu Singh <ms88006218@gmail.com>
2023-11-25 00:15:00 +0530

    Improve

--- script.js
+++ script.js
@@ -7,13 +7,18 @@ const r = document.querySelector(':root');
 function changeMode(){
    
    if(modeSwitch.classList.contains("light")){
-      r.style.setProperty("--theme1","#edf");
-      r.style.setProperty("--theme2","#22222b");
-      r.style.setProperty("--theme1","#edf");
+      r.style.setProperty("--theme1","rgb(175, 175, 183)");
+      r.style.setProperty("--theme2", "#22222b");
+      r.style.setProperty("--theme3","");
+      r.style.setProperty("--trans-theme2","rgba(240, 240, 255, .5)");
+      modeSwitch.classList.remove("light");
    }
    else{
-      r.style.setProperty("--theme2", "#22222b");
-      r.style.setProperty("--theme1","#edf");
+      r.style.setProperty("--theme1","#22222b");
+      r.style.setProperty("--theme2","rgb(175, 175, 183)");
+      r.style.setProperty("--theme3","#0ba284");
+      r.style.setProperty("--trans-theme2","rgba(33, 33, 43, .7)");
+      modeSwitch.classList.add("light");
    }
 }
 modeSwitch.addEventListener("click", changeMode);
