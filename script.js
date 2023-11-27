29d7c2aa1762442d73e6fa8d01fce00cddd6aa80
Sonu Singh <ms88006218@gmail.com>
2023-11-27 15:24:11 +0530

    Bug Fix

--- script.js
+++ script.js
@@ -10,22 +10,24 @@ function changeMode(){
    filler.classList.add("explode");
    setTimeout(() => filler.classList.remove("explode"), 550);
    setTimeout(()=>{
+      modeSwitch.classList.toggle("bi-brightness");
+      modeSwitch.classList.toggle("bi-moon");
 
       if(modeSwitch.classList.contains("light")){
-         r.style.setProperty("--theme1","rgb(175, 175, 183)");
-         r.style.setProperty("--theme2", "#22222b");
-         r.style.setProperty("--theme3","");
-         r.style.setProperty("--trans-theme1","rgba(240, 240, 255, .5)");
-         r.style.setProperty("--trans-theme2","rgba(33, 33, 43, .9)");
+         r.style.setProperty("--theme1","#22222b");
+         r.style.setProperty("--theme2","rgb(175, 175, 183)");
+         r.style.setProperty("--theme3","#0ba284");
+         r.style.setProperty("--trans-theme1","rgba(33, 33, 43, .7)");
+         r.style.setProperty("--trans-theme2","rgba(240, 240, 255, .9)");
          modeSwitch.classList.remove("light");
       }
       else{
-      r.style.setProperty("--theme1","#22222b");
-      r.style.setProperty("--theme2","rgb(175, 175, 183)");
-      r.style.setProperty("--theme3","#0ba284");
-      r.style.setProperty("--trans-theme1","rgba(33, 33, 43, .7)");
-      r.style.setProperty("--trans-theme2","rgba(240, 240, 255, .9)");
-      modeSwitch.classList.add("light");
+         r.style.setProperty("--theme1","rgb(175, 175, 183)");
+         r.style.setProperty("--theme2", "#22222b");
+         r.style.setProperty("--theme3","rgb(225, 48, 86)");
+         r.style.setProperty("--trans-theme1","rgba(240, 240, 255, .5)");
+         r.style.setProperty("--trans-theme2","rgba(33, 33, 43, .9)");
+         modeSwitch.classList.add("light");
       }
    },100)
 }
